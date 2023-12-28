323cc8274f002765a25ec7652e96f19f69423c15
Shahid <Shahid.Shaikh5@ibm.com>
2023-12-28 18:20:32 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,7 +1,14 @@
 import sys
+import platform
+import os
+import sysconfig
 
-print("Python version")
-print(sys.version)
-print("Version info.")
-print(sys.version_info)
-print("Checked Python info successfully.")
+print("Python version              ", sys.version)
+print("Version info.               ", sys.version_info)
+print("platform.architecture()     ", platform.architecture())
+print("os.name                     ", os.name)
+print("sys.platform                ", sys.platform)
+print("platform.system()           ", platform.system())
+print("sysconfig.get_platform()    ", sysconfig.get_platform())
+print("platform.machine()          ", platform.machine())
+print("platform.architecture()     ", platform.architecture())
